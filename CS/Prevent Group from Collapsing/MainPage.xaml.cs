394d19b9cefd3ad6dbb117c10b976c08906f9a39
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Prevent Group from Collapsing/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using DevExpress.Xpf.Grid;
+using DXExample.DemoData;
+
+namespace Prevent_Group_from_Collapsing {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            gridControl1.DataSource = Invoice.GetData();
+        }
+        private void gridControl1_GroupRowCollapsing(object sender, RowAllowEventArgs e) {
+            if(e.Row == null ||
+                ((Invoice)e.Row).Status == InvoiceStatus.Invalidated &&
+                gridControl1.Columns["Status"].GroupIndex != -1)
+                e.Allow = false;
+        }
+    }
+}
